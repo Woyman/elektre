bcdd766fd4480d9e84cdcd2ef779a7ccc97542d4
WOYMAN 1 <woyman11@gmail.com>
2020-01-30 22:36:33 +0700

    add ck editor

--- admin/edit/alternatif.php
+++ admin/edit/alternatif.php
@@ -87,7 +87,7 @@ $alt = mysqli_fetch_assoc($qGetDataAlt);
 
                     <div class="form-group">
                         <label for="merk">Deskripsi</label>
-                        <textarea class="form-control" name="deskripsi" rows="5" Required> <?= $alt['deskripsi'] ?> </textarea>
+                        <textarea class="form-control" name="deskripsi" id="desc" rows="5" Required> <?= $alt['deskripsi'] ?> </textarea>
                     </div>
 
                     <div class="form-group">
@@ -137,6 +137,10 @@ $alt = mysqli_fetch_assoc($qGetDataAlt);
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="../../plugin/ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('desc');
+    </Script>
 
   </body>
 </html>
--- admin/tambahalternatif.php
+++ admin/tambahalternatif.php
@@ -90,7 +90,7 @@ $getKriteria = mysqli_query($konek, "SELECT * FROM $table");
 
                     <div class="form-group">
                         <label for="merk">Deskripsi</label>
-                        <textarea class="form-control" name="deskripsi" rows="5" Required></textarea>
+                        <textarea class="form-control" name="deskripsi" id="desc" rows="5" Required></textarea>
                     </div>
 
                     <div class="form-group">
@@ -127,6 +127,10 @@ $getKriteria = mysqli_query($konek, "SELECT * FROM $table");
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="../plugin/ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('desc');
+    </Script>
 
   </body>
 </html>
